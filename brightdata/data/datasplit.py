16bb986dadc1ba7a8ef1f8e5b38ab9b0f33ae0b0
John Yun Moe <jy4017@nyu.edu>
2026-05-12 00:51:55 +0400

    fixed bug with datasplit.py

--- brightdata/data/datasplit.py
+++ brightdata/data/datasplit.py
@@ -1,6 +1,7 @@
 import json
 import random
 import os
+import shutil
 
 # load all image paths from both folders into separate lists
 ae_images = []
@@ -26,8 +27,27 @@ train_set = ae_images[5000:] + gulf_images[5000:]
 
 print(f'Test: {len(test_set)} | Train: {len(train_set)}')
 
-with open('./data/test_set.json', 'w') as f:
-    json.dump(test_set, f, indent=2)
 
-with open('./data/train_set.json', 'w') as f:
-    json.dump(train_set, f, indent=2)
+test_dir = './test_set'
+train_dir = './train_set'
+
+for folder in [
+    os.path.join(test_dir, 'imagesAE'),
+    os.path.join(test_dir, 'imagesgulf'),
+    os.path.join(train_dir, 'imagesAE'),
+    os.path.join(train_dir, 'imagesgulf'),
+]:
+    os.makedirs(folder, exist_ok=True)
+
+def copy_images(image_paths, output_root):
+    for image_path in image_paths:
+        region = 'imagesAE' if 'imagesAE' in image_path else 'imagesgulf'
+        destination = os.path.join(output_root, region, os.path.basename(image_path))
+        shutil.copy2(image_path, destination)
+
+
+copy_images(test_set, test_dir)
+copy_images(train_set, train_dir)
+
+print(f'Saved test images to {test_dir}')
+print(f'Saved train images to {train_dir}')
